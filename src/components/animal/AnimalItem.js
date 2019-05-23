4cf43da66b0d0e77b7f5b4e8dd138d8c53d7a3d3
Deep Patel <deep.ag123007@gmail.com>
2019-05-23 08:32:09 -0500

    delete and detail get working

--- src/components/animal/AnimalItem.js
+++ src/components/animal/AnimalItem.js
@@ -1,16 +1,31 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import dog from "./dog.jpg";
 
 class AnimalItem extends Component {
+  state = {
+    saveDisabled: false
+  };
+
   handleClick = event => {
     console.log("click", event, this.props.animal.id);
+    this.setState({
+      saveDisabled: true
+    });
     this.props.deleteAnimal(this.props.animal.id);
   };
 
   render() {
     return (
       <article>
+        <img src={dog} className="icon--dog" />
         <h3>{this.props.animal.name}</h3>
-        <button onClick={this.handleClick}>Delete</button>
+        <button onClick={this.handleClick} disabled={this.state.saveDisabled}>
+          Delete
+        </button>
+        <Link className="btn" to={`/animals/${this.props.animal.id}`}>
+          Details
+        </Link>
       </article>
     );
   }
--- src/components/location/LocationItem.js
+++ src/components/location/LocationItem.js
@@ -1,11 +1,31 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import location from "./location.jpg";
 
 class LocationItem extends Component {
+  state = {
+    saveDisabled: false
+  };
+
+  handleClick = event => {
+    console.log("click", event, this.props.location.id);
+    this.setState({
+      saveDisabled: true
+    });
+    this.props.deleteLocation(this.props.location.id);
+  };
+
   render() {
     return (
       <article>
+        <img src={location} className="icon--location" />
         <h3>{this.props.location.name}</h3>
-        <p>{this.props.location.address}</p>
+        <button onClick={this.handleClick} disabled={this.state.saveDisabled}>
+          Delete
+        </button>
+        <Link className="btn" to={`/locations/${this.props.location.id}`}>
+          Details
+        </Link>
       </article>
     );
   }
